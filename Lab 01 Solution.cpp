d1db376c9950720847cc76e79edd3ea9fd628dd5
chandanbraich <43758911+chandanbraich@users.noreply.github.com>
2018-10-01 13:43:21 -0400

    Add files via upload

---
+++ Lab 01 Solution.cpp
@@ -0,0 +1,21 @@
+// Solution for Lab 01
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+   string name;
+   double height, weight;
+    
+   cout << "Please enter the person's name: " << endl;
+   cin >> name;
+   cout << "Please enter the person's height: " << endl;
+   cin >> height;
+   cout << "Please enter the person's weight: " << endl;
+   cin >> weight;
+   
+   double BMI;
+   BMI = height * 703 / (weight * weight);
+   cout << name << "'s BMI is " << BMI << endl;
+}
